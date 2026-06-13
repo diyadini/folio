910e285f43cb6a8d2131d746c333b58e552ea0e2
diyadini <diyatresadini@gmail.com>
2026-06-13 12:00:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,7 +96,7 @@ def send_email(summary_text):
    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
         server.login(sender, password)
         server.send_message(msg)
-        print("Email sent.")
+   print("Email sent.")
  
 
 
